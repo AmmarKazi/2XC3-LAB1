caf2b4e8886f5c84537da9dd241fa76c4d6c1043
Zahin Hossain <82548369+ZahinHossainCS@users.noreply.github.com>
2022-01-21 18:44:03 -0500

    Update code.py
    Zahin Updates code.py

--- code.py
+++ code.py
@@ -1,1 +1,11 @@
+def are_valid_groups(groups, studentNumbers):
+    flatgroups = [element for sublist in groups for element in sublist]
+    check =  all(item in flatgroups for item in studentNumbers)
+    check2 =  all(item in studentNumbers for item in flatgroups)
+    if (check is True and check2 is True):
+        return True  
+    else :
+        return False
+
+print(are_valid_groups([[1,2,3],[4,5,6],[7,8,9]], [1,2,3,4,5,6,7,8,9]))
 
