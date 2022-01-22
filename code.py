5ad8f06e2dc87caf11bc8d1060bf8a07c29bd0d6
Zahin Hossain <82548369+ZahinHossainCS@users.noreply.github.com>
2022-01-21 20:35:09 -0500

    Revert "Update code.py"
    This reverts commit 10e2b4da249e7ac92e012469833647815ba05d01.

--- code.py
+++ code.py
@@ -1,9 +1,10 @@
 def are_valid_groups(groups, studentNumbers):
-    flatgroups = [element for sublist in groups for element in sublist]
-    check =  all(item in flatgroups for item in studentNumbers)
-    check2 =  all(item in studentNumbers for item in flatgroups)
-    if (check is True and check2 is True):
-        return True  
-    else :
+    flatten = [x for y in groups for x in y]
+    check1 =  all(item in flatten for item in studentNumbers)
+    check2 = all(item in studentNumbers for item in flatten)
+    if check1 and check2 == True:
+        return True
+    else:
         return False
-print(are_valid_groups([[1,2,3],[4,5,6],[7,8,9]], [1,2,3,4,5,6,7,8,9]))
+
+print(are_valid_groups([[1,2,3],[4,5,6],[7,8]], [1,2,3,4,5,6,7,8,9]))
