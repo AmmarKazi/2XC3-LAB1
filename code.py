89db679130cfbdce913de70266a941886a4b6f11
Ammar Kazi <ammarkazi@Ammars-MacBook-Air.local>
2022-01-21 21:14:46 -0500

    Change 2

--- code.py
+++ code.py
@@ -2,8 +2,8 @@ def are_valid_groups(groups, studentNumbers):
     flatten = [x for y in groups for x in y]
     check1 =  all(item in flatten for item in studentNumbers)
     check2 = all(item in studentNumbers for item in flatten)
-    if check1 and check2 == True:
-        return True
+    if check1 and check2 == False:
+        return False
     else:
         return False
-print(are_valid_groups([[1,2,3],[4,5,6],[7,8,9]], [1,2,3,4,5,6,7,8,9]))›
+print(are_valid_groups([[1,2,3],[4,5,6],[7,8,9]], [1,2,3,4,5,6,7,8,9]))
