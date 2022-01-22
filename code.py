b6b7a6298508d6ad45a757e3381db4ca250740f0
Zahin Hossain <82548369+ZahinHossainCS@users.noreply.github.com>
2022-01-21 20:50:55 -0500

    Update code.py
    final update

--- code.py
+++ code.py
@@ -1,8 +1,9 @@
 def are_valid_groups(groups, studentNumbers):
-    nest = [i for group in groups for i in group]
-    ans = all(student in nest for student in students)
-
-    if ans:
+    flatgroups = [element for sublist in groups for element in sublist]
+    check =  all(item in flatgroups for item in studentNumbers)
+    check2 =  all(item in studentNumbers for item in flatgroups)
+    if (check is True and check2 is True):
         return True  
-    else:
+    else :
         return False
+print(are_valid_groups([[1,2,3],[4,5,6],[7,8,9]], [1,2,3,4,5,6,7,8,9]))
